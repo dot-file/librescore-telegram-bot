c4bde9ea9b391d24bb25a4f3543dfcf1f8854e67
dot-file <Random145@tutamail.com>
2025-08-20 08:25:41 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Telegram bot that allows you to download pdf, midi and mp3 from musescore.com";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
